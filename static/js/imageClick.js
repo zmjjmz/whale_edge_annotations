e1ae6c35b0f8f381c66941238e0d5a925c3284ec
batboa <batboa@rpi.edu>
2015-10-06 12:56:48 -0400

    First algo implementation

--- static/js/imageClick.js
+++ static/js/imageClick.js
@@ -1,3 +1,85 @@
+function zeros(dimensions){
+  var array = [];
+  for (var i = 0; i < dimensions[0]; ++i) {
+        array.push(dimensions.length == 1 ? 0 : zeros(dimensions.slice(1)));
+    }
+
+    return array;
+}
+
+function setPassThrough(array, pt){
+  for(var i = 0; i < array.length; i++){
+    array[i][pt[0]] = Number.NEGATIVE_INFINITY;
+  }
+  array[pt[1]][pt[0]] = 0;
+}
+
+function getCost(row, col, i, gradient_y_image, cost){
+  if(row + i < 0 || row+1 >= array.length){
+    return Number.NEGATIVE_INFINITY;
+  }
+  else{
+    return cost[row+i][col-1] + gradient_y_image[row,col];
+  }
+}
+
+function getCandidates(row, col, gradient_y_image, cost, neighbor_range){
+  var candidates = [];
+  for(i in neighbor_range){
+    candidates.push(getCost(row,col,i,gradient_y_image,cost))
+  }
+  return candidates;
+}
+
+function argMax(candidates){
+  var maxVal = candidates[0];
+  var maxIndex = 0;
+  for(var i = 1; i < candidates.length; i++){
+    if(maxVal < candidates[i]){
+      maxVal = candidates[i];
+      maxIndex = i;
+    }
+  }
+  return maxIndex;
+}
+
+function find_seam(yGradient, start,end, n_neighbors){
+  alert("STARTING");
+  if(n_neighbors % 2 != 1){
+    alert("n_neighbors is not an odd number");
+    return;
+  }
+  var neighbor_range = []
+  for(var i = -1 * Math.floor(n_neighbors/2); i < 1 + Math.floor(n_neighbors/2); i++){
+    neighbor_range.push(i);
+  }
+  yGradient = setPassThrough(yGradient, start)
+  yGradient = setPassThrough(yGradient, end)
+  //TODO be able to pick additional points
+  var cost = zeros([yGradient.length, yGradient[0].length ])
+  var back = zeros([yGradient.length, yGradient[0].length ])
+  //TODO check start is before end
+  for(var col = start[0]; col < end[0] + 1; col++){
+    for(var row = 0; row < yGradient.length; row++){
+      candidates = getCandidates(row, col, yGradient, cost, neighbor_range);
+      var best = argMax(candidates);
+      back[row][col] = best - Math.floor(n_neighbors / 2);
+      cost[row][col] =  candidates[best];
+    }
+  }
+  var path = [];
+  var curr_y = end[1];
+  var path_cost = 0;
+  for(var col = end[0]+1; col >= start[0]; col--){
+    path_cost += cost[curr_y,col];
+    path.append(curr_y);
+    next_ = curr_y + back[curr_y,col];
+    curr_y = next_;
+  }
+  alert("DONE");
+  return path;
+}
+
 function updateModal(data){
   var modalData = $('#mainModalData');
   modalData.empty();
@@ -6,7 +88,6 @@ function updateModal(data){
     point.text(data[i]);
     modalData.append(point);
   }
-
 }
 
 function updateMainImage(){
@@ -37,6 +118,7 @@ $(document).ready(function(e) {
   var endPoint = false;
   var endLocaiton;
   var imageData;
+  var path = false;
   $('.displayed').click(function(e) {
     var offset = $('.displayed').offset();
     var posX = e.pageX - offset.left,posY = e.pageY - offset.top;
@@ -66,9 +148,13 @@ $(document).ready(function(e) {
       updateModal(Ptdata);
       $('#TheModal').modal('show');
   });
-  
+
   $('#submitData').click(function(e){
       if(startPoint && endPoint){
+
+        setTimeout( path = find_seam(yGradient, startLocaiton,endLocaiton, 3), 0 );
+
+        /*
       	var toSubmit = {'id':$('#mainImage').attr("alt"), 'points':Ptdata};
       	Ptdata = [];
         startPoint = false;
@@ -81,9 +167,10 @@ $(document).ready(function(e) {
     		dataType : 'json',
     		data : JSON.stringify(toSubmit),
     		complete : function(result) {
-                  
+
               }
         });
+        */
       }
       else{
       	alert("Please Label start and end points before submitting");
